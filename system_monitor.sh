427a9021d758cf6ed98873e94d9a95338aa3054c
mitroph <i.am.mitroph@gmail.com>
2026-08-11 08:38:48 +0000

    refactored bash script for better readability

--- system_monitor.sh
+++ system_monitor.sh
@@ -1,1 +1,15 @@
-echo "$(date '+%A %W %Y %X') $(ps aux | sort -nk +4 | tail -n 5) $(df -h)" > report.txt
+#!/bin/bash
+
+# Clear file and rewrote header with date and time
+echo "=== System Report ===" > report.txt
+date '+%A %W %Y %X' >> report.txt
+echo "" >> report.txt
+
+## Adding top 5 processes by RAM consumption
+echo "--- Top 5 RAM consuming processes ---" >> report.txt
+ps aux | sort -nk 4 | tail -n 5 >> report.txt
+echo "" >> report.txt
+
+# Adding disk usage status
+echo "--- Disk Usage ---" >> report.txt 
+df -h >> report.txt
